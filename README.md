d0f131f3e4910c9518b16a1bb969234dc6737df5
Jhair Escobar <je@todoo.co>
2021-11-29 16:57:52 -0500

    [FIX] wkhtmltopdf error

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mujeron
+# rebuild for wkhtmltopdf error
---
+++ stock_extended/models/account_move.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
